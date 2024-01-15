96fe7c5b294cdcd4c3d9d9e53e5940ae25914603
duck82father <legnaevol84@gmail.com>
2024-01-15 10:50:22 +0900

    03 페이징 및 게시물 번호지정

--- src/main/java/kr/or/ysedu/mybo2/answer/AnswerController.java
+++ src/main/java/kr/or/ysedu/mybo2/answer/AnswerController.java
@@ -1,13 +1,12 @@
 package kr.or.ysedu.mybo2.answer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import ch.qos.logback.core.model.Model;
 import jakarta.validation.Valid;
 import kr.or.ysedu.mybo2.question.Question;
 import kr.or.ysedu.mybo2.question.QuestionService;
@@ -28,6 +27,7 @@ public class AnswerController {
 			@Valid AnswerForm answerForm, BindingResult bindingResult) {
 		Question question = this.questionService.getQuestion(id);
 		if (bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
 			return "question_detail";
 		}
 		this.answerService.create(question, answerForm.getContent());
--- src/test/java/kr/or/ysedu/mybo2/Mybo2ApplicationTests.java
+++ src/test/java/kr/or/ysedu/mybo2/Mybo2ApplicationTests.java
@@ -8,20 +8,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import kr.or.ysedu.mybo2.question.Question;
 import kr.or.ysedu.mybo2.question.QuestionRepository;
+import kr.or.ysedu.mybo2.question.QuestionService;
 
 @SpringBootTest
 class Mybo2ApplicationTests {
 
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 	
 	@Test
-	void contextLoads() {
-		Question q1 = new Question();
-		q1.setSubject("첫번째 게시물");
-		q1.setContent("내용없음");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);		
+	void testJpa() {
+		for (int i=0; i<300; i++) {
+			String subject = String.format("%03d 번째 게시물 입니다.", i);
+			String content = "내용 없음";
+			this.questionService.create(subject, content);
+		}
 	}
-
 }
