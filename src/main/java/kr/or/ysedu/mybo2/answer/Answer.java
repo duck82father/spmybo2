9ab6312e51abe6d4f72d7c96226af539d263c645
duck82father <legnaevol84@gmail.com>
2024-01-16 16:24:15 +0900

    10 추천 기능

--- src/main/java/kr/or/ysedu/mybo2/answer/Answer.java
+++ src/main/java/kr/or/ysedu/mybo2/answer/Answer.java
@@ -1,12 +1,14 @@
 package kr.or.ysedu.mybo2.answer;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import kr.or.ysedu.mybo2.question.Question;
 import kr.or.ysedu.mybo2.user.SiteUser;
@@ -34,5 +36,8 @@ public class Answer {
 	private SiteUser author;
 	
 	private LocalDateTime modifyDate;
+	
+	@ManyToMany
+	Set<SiteUser> voter;
 	
 }
--- src/main/java/kr/or/ysedu/mybo2/question/Question.java
+++ src/main/java/kr/or/ysedu/mybo2/question/Question.java
@@ -2,6 +2,7 @@ package kr.or.ysedu.mybo2.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -9,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import kr.or.ysedu.mybo2.answer.Answer;
@@ -41,4 +43,6 @@ public class Question {
 	
 	private LocalDateTime modifyDate;
 	
+	@ManyToMany
+	Set<SiteUser> voter;
 }
